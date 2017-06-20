79eda680b8e2e949bb72c15f188d06384d481abd
kontur32 <kontur32@yandex.ru>
2017-06-21 00:30:13 +0300

    Четвертая

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+if (file_exists('loader.php')) {
+    require_once  'loader.php';
+}
+ else {
+echo "Отправь смс! <br/>"
+.'Читайте <a href ="http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'/readme.txt">README.md</a>' ;
+
+ }
