516e6f7ed7fffdecb9dc3a27709b44c29f496ef3
kontur32 <kontur32@yandex.ru>
2018-02-03 15:42:35 +0300

    добалена запись логов запросов

--- loader.php
+++ loader.php
@@ -17,8 +17,19 @@ Registry::getInstance()->setResource('setting',
 
 $app_path = Registry::getInstance()->getResource('setting')['app_path'];
 $includes_path = Registry::getInstance()->getResource('setting')['includes_path'];
+$data_path = Registry::getInstance()->getResource('setting')['data_path'];
+$logs_path = Registry::getInstance()->getResource('setting')['logs_path'];
 
 require_once $app_path . 'router.php';
 require_once $includes_path . 'password.php';
 
 Route::start();
+
+// запись запроса в лог
+$log_rec = $_SERVER["REMOTE_ADDR"] . ' - - [' 
+        . date('j/m/y:h:i:s', $_SERVER["REQUEST_TIME"]). '] ' 
+        . '"' . $_SERVER["REQUEST_METHOD"] . ' '. $_SERVER['REQUEST_URI'] . $_SERVER["QUERY_STRING"] . '" '
+        . $_SERVER["REMOTE_PORT"] . ' '
+        . '"' . $_SERVER["HTTP_USER_AGENT"] . '"'
+        . "\n";
+file_put_contents ('logs/sms.access.log', $log_rec, FILE_APPEND); 
